7506a5cc62e6188b036b4357877084b41dffccdc
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2025-06-14 20:47:31 -0400

    Initial commit

---
+++ tests/Customers.Api.Tets.Integration/TestCollection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customers.Api.Tets.Integration
+{
+    [CollectionDefinition("CustomerApi Collection")]
+    public class TestCollection : ICollectionFixture<WebApplicationFactory<IApiMarker>>
+    {
+    }
+}
