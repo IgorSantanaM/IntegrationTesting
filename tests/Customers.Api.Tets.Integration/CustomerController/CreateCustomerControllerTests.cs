e4d4f330098d9fd8471a16c061ad419bd08cf609
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2025-06-17 19:22:54 -0400

    (feat): Added first integration testing

--- tests/Customers.Api.Tets.Integration/CustomerController/CreateCustomerControllerTests.cs
+++ tests/Customers.Api.Tets.Integration/CustomerController/CreateCustomerControllerTests.cs
@@ -34,7 +34,35 @@ namespace Customers.Api.Tets.Integration.CustomerController
         [Fact]
         public async Task Create_CreatesUser_WhenDataIsValid()
         {
-            await Task.Delay(5000);
+            // Arrange
+            var customer =  _customerGenerator.Generate();
+
+            // Act
+            var response = await _client.PostAsJsonAsync("customers", customer);
+
+            // Assert
+            var customerResponse = response.Content.ReadFromJsonAsync<CustomerResponse>();
+
+            customerResponse.Should().BeEquivalentTo(customer, options => options.ExcludingMissingMembers());
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+            response.Headers.Location!.ToString().Should().Be($"http://localhost/customer/{customerResponse.Id!}");
+        }
+
+        [Fact]
+        public async Task Create_ReturnsValidationError_WhenEmailIsInvalid()
+        {
+            // Arrange
+            const string invalidEmail = "dasdadwdads";
+            var customer = _customerGenerator.Clone()
+                    .RuleFor(x => x.Email, invalidEmail).Generate();
+            // Act
+            var response = await _client.PostAsJsonAsync("customers", customer); 
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var errors = await response.Content.ReadFromJsonAsync<Dictionary<string, string[]>>();
+            errors.Should().ContainKey("Email");
+            errors["Email"].Should().Contain("The Email field is not a valid e-mail address.");
         }
     }
 }
